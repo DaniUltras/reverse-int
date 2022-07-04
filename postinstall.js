93bb7f543fc39cb90f50ce01b7280080ef4051b9
Daniil <daniil_sinkevich99@mail.ru>
2022-07-04 18:28:05 +0300

    feat: add the solution

--- postinstall.js
+++ postinstall.js
@@ -1,13 +1,4 @@
-const semver = require('semver');
-const colors = require('colors/safe');
-
-const { engines: { node: nodeVersion }} = require('./package');
-
-if (!semver.satisfies(process.version, nodeVersion)) {
-  process.emitWarning(
-    colors.red(`
-      For this task we are strictly recomend you to use node ${nodeVersion}.
-      Now you are using node ${process.version}, if you are using any of features that not supported by node ${nodeVersion}, score won't be submitted
-    `)
-  );
+const reverseNumbers = (number) => {
+  let newNumber = [...number.toString()].map(Number).reverse().join('')
+  return newNumber
 }
